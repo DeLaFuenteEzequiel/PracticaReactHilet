ff956eddea6d691a10a3e2e202d2a6d3a7ecadab
Ezequiel de la Fuente <133174623+DeLaFuenteEzequiel@users.noreply.github.com>
2023-11-06 21:50:23 -0300

    props

---
+++ algoritmos-3-frontend-vite/src/Layouts/Inicio.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+
+const Inicio = (props) => {
+
+    return(
+        <section className="w-[100vw] flex justify-center">
+            <menu className="w-[100vw] h-[3rem] absolute bg-slate-500 shadow-sm flex justify-evenly items-center text-white">
+                <Link to='/users' replace={true}>Buscar Usuarios</Link>
+                <Link to='/createUser' replace={true}>Crear Usuarios</Link>
+                <Link to='/createUser' replace={true}>Crear Usuarios</Link>
+                <Link to='/createUser' replace={true}>Crear Usuarios</Link>
+            </menu>
+            <div className="mt-[4rem]">
+                {props.children}
+            </div>
+        </section>
+    );
+}
+
+export default Inicio;
