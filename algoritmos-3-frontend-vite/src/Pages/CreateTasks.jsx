393d7532510bc89e6531f41b3eb2acc0178d17ba
Ezequiel de la Fuente <133174623+DeLaFuenteEzequiel@users.noreply.github.com>
2023-11-08 02:23:37 -0300

    Boostrap ok

--- algoritmos-3-frontend-vite/src/Pages/CreateTasks.jsx
+++ algoritmos-3-frontend-vite/src/Pages/CreateTasks.jsx
@@ -3,13 +3,56 @@ import { useEffect, useState } from "react";
 
 
 const CreateTasks = () => {
-  return (
-    <div>
-      <h1>Formulario para tarea nueva</h1>
-      
-    </div>
+    return (
+        <html lang="en">
+          <head>
+            <meta charSet="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+            <link
+              rel="stylesheet"
+              href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+              integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N"
+              crossOrigin="anonymous"
+            />
+            
+          </head>
+          <body>
+            <h1>Nueva Tarea</h1>
+           
+            <div className="container">
+              <div className="row">
+                <div className="col">
+               
+                <form>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Descripcion</label>
+                        <input type="text" class="form-control" id="descripcion"/>
+                        
+                    </div>
+                   
+                    
+                    <button type="submit" class="btn btn-primary">Agregar Tarea</button>
+                </form>
+
+                          </div>
+                        </div>
+                      </div>
+                      
     
-  );
-};
-console.log("esto funciona");
+                      
+            <script
+              src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+              integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+              crossOrigin="anonymous"
+            ></script>
+            <script
+              src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
+              integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct"
+              crossOrigin="anonymous"
+            ></script>
+          </body>
+        </html>
+      );
+    };
+
 export default CreateTasks;
